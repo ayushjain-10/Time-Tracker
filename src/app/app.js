2c88738d929d163456469c83d60c3541c2f2d8d3
Ayush Jain <ayushshreeshreemal@gmail.com>
2023-05-07 22:21:41 -0700

    added Timer reducers

---
+++ src/app/app.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import timersReducer from '../features/timers/timersSlice'
+export const store = configureStore({
+	reducer: {
+		timers: timersReducer
+	}
+})
