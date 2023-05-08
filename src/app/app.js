17bb5202b545610ce2a193ed6ab3b1811ba3de0b
Ayush Jain <ayushshreeshreemal@gmail.com>
2023-05-07 23:12:39 -0700

    persisting timers

--- src/app/app.js
+++ src/app/app.js
@@ -1,18 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import timersReducer from '../features/timers/timersSlice'
 import { update } from '../features/timers/timersSlice'
+import { loadState, saveState } from '../app/utils/persistState'
+import throttle from 'lodash/throttle'
 
 let lastUpdateTime = Date.now()
 
 setInterval(() => {
-	const now = Date.now()
-	const deltaTime = now - lastUpdateTime
-	lastUpdateTime = now
-	store.dispatch(update(deltaTime))
+    const now = Date.now()
+    const deltaTime = now - lastUpdateTime
+    lastUpdateTime = now
+    store.dispatch(update(deltaTime))
 }, 500)
 
 export const store = configureStore({
-	reducer: {
-		timers: timersReducer
-	}
+    reducer: {
+        timers: timersReducer
+    },
+    preloadedState: loadState() // 5
 })
+
+store.subscribe(throttle(() => {
+    saveState(store.getState())
+  }, 1000));
