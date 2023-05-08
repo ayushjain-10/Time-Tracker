446b2b4f398dddd1c878d88e9bc3046522b9a94a
Ayush Jain <ayushshreeshreemal@gmail.com>
2023-05-07 22:43:32 -0700

    timers running'

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import timersReducer from '../features/timers/timersSlice'
+import { update } from '../features/timers/timersSlice'
+
+let lastUpdateTime = Date.now()
+
+setInterval(() => {
+	const now = Date.now()
+	const deltaTime = now - lastUpdateTime
+	lastUpdateTime = now
+	store.dispatch(update(deltaTime))
+}, 500)
+
 export const store = configureStore({
 	reducer: {
 		timers: timersReducer
